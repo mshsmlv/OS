c52f8caf5325184527d997c4e870c5fa9a44a1bd
MashaSamoylova <mashanedyak@gmail.com>
2018-12-01 15:36:03 +0700

    Add printf

--- kernel.c
+++ kernel.c
@@ -1,11 +1,46 @@
-void main() {
-    char helloC[20] = "Hello C world!!!";
-    char* video_memory = (char*) 0xb8000;
+char* VIDEO_MEMORY = (char*)0xb8000;
+
+int carriage_return() {
+    VIDEO_MEMORY = VIDEO_MEMORY + 80*2 - ((int)VIDEO_MEMORY - 0xb8000)%(80*2);
+}
+
+int tabulation() {
+    if(
+            (*(VIDEO_MEMORY - 2)) != ' ' && 
+            (*(VIDEO_MEMORY - 2)) != '\n' ) {
+        return;
+    }
+
     int i = 0;
-    for(i = 0; i < 17; i++) {
-        *video_memory = helloC[i];
-        *(video_memory + 1) = 0xd;
-        video_memory += 2;
+    while(((int)VIDEO_MEMORY - 0xb8000)%(80*2) && i < 4) {
+        i++;
+        VIDEO_MEMORY++;
     }
 }
+
+int printf(char* format_string) {
+        int i = 0;
+        while (format_string[i]) {
+            if (format_string[i] == '\n') {
+                carriage_return(); 
+                i++;
+                continue;
+            }
+            if (format_string[i] == '\t') {
+                tabulation();
+                i++;
+                continue;
+            }
+            *VIDEO_MEMORY = format_string[i];
+            *(VIDEO_MEMORY + 1) = 0xd;
+            VIDEO_MEMORY += 2;
+            i++;
+        }
+}
+
+void main() {
+    printf("first\n\0");
+    printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \taaaaaaaaaaaaaa\0");
+}
+
 

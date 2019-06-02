a71183c97c6b7a831316f3221c5fdd796c3ce509
MashaSamoylova <mashanedyak@gmail.com>
2019-06-02 14:03:53 +0700

    Add checking for keyboard

--- periphery/keyboard/keyboard.c
+++ periphery/keyboard/keyboard.c
@@ -7,17 +7,24 @@
 char scancode_to_ascii[100] = "\xff`1234567890-=\xff\xffqwertyuiop[]\xff\xff\x61sdfghjkl;'\x0a\xff\xffzxcvbnm,./\xff\xff\xff\xff ";
 
 void keyboard_callback(stack_with_err_code regs) {
-    char letter;
-    unsigned int scan_code = read_byte_from_port(0x60);
-    io_wait();
-    if(scan_code < 59) {
-        if (scan_code == 28) {
-            print("\n");
-            return;
-        }
-        letter = scancode_to_ascii[scan_code];
-        if (letter != 0xff) {
-            print_char(letter);
+    unsigned int status = read_byte_from_port(0x64);
+    // Check read enable from -x60 port
+    // and that ps/2 connector is clear.
+    // If ps/2 connector is set, it means that
+    // in the port 0x60 mouse data.
+    if ((status & 0x1) && !(status & 0x20)) {
+        char letter;
+        unsigned int scan_code = read_byte_from_port(0x60);
+        io_wait();
+        if(scan_code < 59) {
+            if (scan_code == 28) {
+                print("\n");
+                return;
+            }
+            letter = scancode_to_ascii[scan_code];
+            if (letter != 0xff) {
+                print_char(letter);
+            }
         }
     }
 }
